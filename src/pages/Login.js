6b27a33e6a7c9c5a359c115af68815fa255a2ba4
Mac <mac@MacBook-Pro-de-Mac.local>
2022-08-12 00:44:48 +0100

    first commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -53,7 +53,7 @@ yn
 
     const connection = (e)=>
     {
-        fetch('http://127.0.0.1:8000/api/login/', {
+        fetch('https://modelisationfiscaleapi/api/login/', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(state.credentials)

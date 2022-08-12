5ed25daf4886cb4f163352f6f5365a64312a48f3
Mac <mac@MacBook-Pro-de-Mac.local>
2022-08-12 21:21:23 +0100

    first commit4

--- src/pages/PopulationTolalProvinceInfo.js
+++ src/pages/PopulationTolalProvinceInfo.js
@@ -61,13 +61,13 @@ function PopulationTotalProvinceInfo(props)
    
           const total_general_split_urbain = total_taille_population_final.reduce((total,value)=>
           {
-            total = total + Number(value.total_split_urbain).toFixed()
+            total = Number(total + Number(value.total_split_urbain)).toFixed()
             return total
           },0)
 
           const total_general_split_rural = total_taille_population_final.reduce((total,value)=>
           {
-            total = total + Number(value.total_split_rural).toFixed()
+            total = Number(total + Number(value.total_split_rural)).toFixed()
             return total
           },0)
           

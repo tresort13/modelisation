3f54a4e02578d1fbc76e83b3a670f173692006ff
Mac <mac@MacBook-Pro-de-Mac.local>
2022-12-12 21:25:31 -0800

    commit5

--- src/pages/MenuRecettes.js
+++ src/pages/MenuRecettes.js
@@ -22,9 +22,9 @@ function MenuRecettes(props)
     return (
     <>
      <Header username={props.username}/>
-       {isDesktop && <Container className=" mb-5" style={{marginTop:50}}>
+       {isDesktop && <Container className="mt-2 mb-5" >
        <Row className="text-center justify-content-center">
-            <Col md={4} className="my-auto text-center">
+            <Col md={6} className="my-auto text-center">
             <Link to="/menu_dgi" style={{color:'white',textDecorationLine:'none'}}>
             <Button variant="warning" style={{width:300,height:300}} className='btn-lg rounded-pill zoom'>
             <i className="text-secondary"><b>D G I</b></i>
@@ -32,7 +32,7 @@ function MenuRecettes(props)
             </Link>    
             </Col>
 
-            <Col mdmd={4} className="my-auto text-center">
+            <Col mdmd={6} className="my-auto text-center">
             <Link to="/menu_dgda" style={{color:'white',textDecorationLine:'none'}}>
             <Button variant="warning" style={{width:300,height:300}} className='btn-lg rounded-pill zoom'>
             <i className="text-secondary"><b>D G D A</b></i>
@@ -42,7 +42,7 @@ function MenuRecettes(props)
       </Row>
 
         <Row className="text-center justify-content-center mt-3">
-            <Col md={4} className="my-auto text-center">
+            <Col md={6} className="my-auto text-center">
             <Link to="/menu_dgrad" style={{color:'white',textDecorationLine:'none'}}>
             <Button variant="warning" style={{width:300,height:300}} className='btn-lg rounded-pill zoom'>
             <i className="text-secondary"><b>D G R A D</b></i>
@@ -50,7 +50,7 @@ function MenuRecettes(props)
              </Link>     
             </Col>
 
-            <Col md={4} className="my-auto text-center">
+            <Col md={6} className="my-auto text-center">
             <Link to="/menu_recettes_publiques" style={{color:'white',textDecorationLine:'none'}}>
             <Button variant="warning" style={{width:300,height:300}} className='btn-lg rounded-pill zoom'>
             <i className="text-secondary"><b>Recettes publiques</b></i>

cc6d9441016e3709fbd171dd821032b2d77efb7c
Mac <mac@MacBook-Pro-de-Mac.local>
2022-12-14 23:25:43 -0800

    commit5

--- src/pages/ImpotDGIInfo.js
+++ src/pages/ImpotDGIInfo.js
@@ -49,7 +49,7 @@ const autre_impots_annee_fiscale_2022_temp1 = Number( Number(props.impotDGIInfo[
 const autre_impots_annee_fiscale_2022_temp2 = Number(Number(props.impotDGIInfo[1].annee_fiscale_2020) / Number(props.impotDGIInfo[2].annee_fiscale_2020)).toFixed(2)
 const autre_impots_annee_fiscale_2022_temp3 = Number(Number(props.impotDGIInfo[1].annee_fiscale_2019) / Number(props.impotDGIInfo[2].annee_fiscale_2019)).toFixed(2)
 const autre_impots_annee_fiscale_2022_average = Number((Number(autre_impots_annee_fiscale_2022_temp1) + Number(autre_impots_annee_fiscale_2022_temp2) + Number(autre_impots_annee_fiscale_2022_temp3)) / 3)
-const pib_annee_fiscale_2022= Number(Number(props.impotDGIInfo[2].annee_fiscale_2021) * (1 + Number(props.impotTauxCroissance).toFixed(1))).toFixed()
+const pib_annee_fiscale_2022= Number(Number((props.impotDGIInfo[2].annee_fiscale_2021) * (1 + Number(props.impotTauxCroissance)))).toFixed()
 
 const autre_impots_annee_fiscale_2022_final = Number(Number(pib_annee_fiscale_2022) * Number(autre_impots_annee_fiscale_2022_average)).toFixed()
 

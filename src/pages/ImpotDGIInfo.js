f7609e2d45e5dc1703e2bb476d930f22b8e2fe99
Mac <mac@MacBook-Pro-de-Mac.local>
2022-12-14 21:55:27 -0800

    commit5

--- src/pages/ImpotDGIInfo.js
+++ src/pages/ImpotDGIInfo.js
@@ -45,6 +45,9 @@ const ca_annee_fiscale_2022_temp3 = Number(Number(props.impotDGIInfo[0].annee_fi
 const ca_annee_fiscale_2022_average = Number((Number(ca_annee_fiscale_2022_temp1) + Number(ca_annee_fiscale_2022_temp2) + Number(ca_annee_fiscale_2022_temp3)) / 3).toFixed(2)
 const ca_annee_fiscale_2022_final = Number(Number(props.impotDGIInfo[2].annee_fiscale_2021 ) * Number(ca_annee_fiscale_2022_average)).toFixed()
 impotDGI = [...props.impotDGIInfo]
+impotDGI[0].annee_fiscale_2022 = ca_annee_fiscale_2022_final
+impotDGI[1].annee_fiscale_2022 = 0
+impotDGI[2].annee_fiscale_2022 = 0
 
 console.log(impotDGI)
 //impotDGI[0].annee_fiscale_2022=ca_annee_fiscale_2022_final
@@ -86,7 +89,7 @@ console.log(impotDGI)
              <td><i><b>{new Intl.NumberFormat().format(Number(value.annee_fiscale_2019).toFixed())}</b></i></td>
              <td><i><b>{new Intl.NumberFormat().format(Number(value.annee_fiscale_2020).toFixed())}</b></i></td>
              <td><i><b>{new Intl.NumberFormat().format(Number(value.annee_fiscale_2021).toFixed())}</b></i></td>
-             <td><i><b>{new Intl.NumberFormat().format(Number(ca_annee_fiscale_2022_final).toFixed())}</b></i></td>
+             <td><i><b>{new Intl.NumberFormat().format(Number(value.annee_fiscale_2022).toFixed())}</b></i></td>
             </tr>     
         }) 
         }

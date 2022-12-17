f39225a21cdf7832f7860e3edf026a67b29dd833
Mac <mac@MacBook-Pro-de-Mac.local>
2022-12-17 13:13:09 -0800

    commit5

--- src/pages/FormDGI.js
+++ src/pages/FormDGI.js
@@ -1,4 +1,4 @@
-import React,{useRef} from 'react';
+import React,{useRef,useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import Button from "react-bootstrap/Button";

0a552ed7f67751650d619216ee09b3dcac03168c
Mac <mac@MacBook-Pro-de-Mac.local>
2022-12-17 02:15:20 -0800

    commit5

--- src/App.js
+++ src/App.js
@@ -518,7 +518,7 @@ return (
     <Route path="/impot_dgi_info" element={<ImpotDGIInfo username = {username} impotDgiInfo={impotDgiInfo} impotPourcentageCroissance={impotPourcentageCroissance}/>} >
     </Route>
 
-    <Route path="/impo_expo_dgda_info" element={<ImpoExpoDGDAInfo username = {username} infoExpoImpo={infoExpoImpo} tauxExportation={tauxExportation} tauxImportation={tauxImportation} impotDGIInfo={impotDGIInfo} impotTauxCroissance={impotTauxCroissance}/>} >
+    <Route path="/impo_expo_dgda_info" element={<ImpoExpoDGDAInfo username = {username} infoExpoImpo={infoExpoImpo} tauxExportation={tauxExportation} tauxImportation={tauxImportation}/>} >
     </Route>
 
     
